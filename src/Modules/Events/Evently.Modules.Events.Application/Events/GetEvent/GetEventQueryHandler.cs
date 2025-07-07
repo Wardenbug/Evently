a064778ba745ad75db6a8a896027130295b4227f
Wardenbug <danylo.storozhuk.ua@gmail.com>
2025-07-07 20:24:17 +0200

    refactor events module to clean architecture

---
+++ src/Modules/Events/Evently.Modules.Events.Application/Events/GetEvent/GetEventQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using System.Data.Common;
+using Dapper;
+using Evently.Modules.Events.Application.Abstractions.Data;
+using MediatR;
+
+namespace Evently.Modules.Events.Application.Events.GetEvent;
+
+internal sealed class GetEventQueryHandler(IDbConnectionFactory dbConnectionFactory) : IRequestHandler<GetEventQuery, EventResponse?>
+{
+    public async Task<EventResponse?> Handle(GetEventQuery request, CancellationToken cancellationToken)
+    {
+        await using DbConnection connection = await dbConnectionFactory.OpenConnectionAsync();
+
+        const string sql =
+            $"""
+            SELECT      
+                id as {nameof(EventResponse.Id)}
+                title as {nameof(EventResponse.Title)}
+                description as {nameof(EventResponse.Description)}
+                location as {nameof(EventResponse.Location)}
+                starts_at_utc as {nameof(EventResponse.StartsAtUtc)}
+                ends_at_utc as {nameof(EventResponse.EndsAtUtc)}
+            FROM events.events
+            WHERE id = @EventId
+            """;
+
+        EventResponse? @event = await connection.QuerySingleOrDefaultAsync(sql, request);
+
+        return @event;
+    }
+}
---
+++ src/Modules/Events/Evently.Modules.Events.Presentation/Events/EventEndpoints.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Routing;
+
+namespace Evently.Modules.Events.Presentation.Events;
+public static class EventEndpoints
+{
+    public static void MapEndpoins(IEndpointRouteBuilder app)
+    {
+        CreateEvent.MapEndpoints(app);
+        GetEvent.MapEndpoint(app);
+    }
+}
