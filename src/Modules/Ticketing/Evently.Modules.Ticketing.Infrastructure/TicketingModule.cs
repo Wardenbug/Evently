5bf69f07e732de36dec82e2f4f60efa66213e337
Wardenbug <danylo.storozhuk.ua@gmail.com>
2025-07-17 18:56:12 +0200

    add synchonous in-memory communication between modules

--- src/Modules/Ticketing/Evently.Modules.Ticketing.Infrastructure/TicketingModule.cs
+++ src/Modules/Ticketing/Evently.Modules.Ticketing.Infrastructure/TicketingModule.cs
@@ -1,6 +1,7 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Evently.Common.Presentation.Endpoints;
+using Evently.Modules.Ticketing.Application.Carts;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Evently.Common.Presentation.Endpoints;
 
 namespace Evently.Modules.Ticketing.Infrastructure;
 
@@ -17,8 +18,11 @@ public static class TicketingModule
         return services;
     }
 
-    private static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    private static void AddInfrastructure(this IServiceCollection services,
+#pragma warning disable S1172 // Unused method parameters should be removed
+        IConfiguration configuration)
+#pragma warning restore S1172 // Unused method parameters should be removed
     {
-        // for future
+        services.AddSingleton<CartService>();
     }
 }
---
+++ src/Modules/Users/Evently.Modules.Users.Infrastructure/PublicApi/UsersApi.cs
@@ -0,0 +1,25 @@
+﻿using Evently.Common.Domain;
+using Evently.Modules.Users.Application.Users.GetUser;
+using Evently.Modules.Users.PublicApi;
+using MediatR;
+
+namespace Evently.Modules.Users.Infrastructure.PublicApi;
+internal sealed class UsersApi(ISender sender) : IUsersApi
+{
+    public async Task<Modules.Users.PublicApi.UserResponse?> GetAsync(Guid userId, CancellationToken cancellationToken = default)
+    {
+        Result<Application.Users.GetUser.UserResponse> result =
+            await sender.Send(new GetUserQuery(userId), cancellationToken);
+
+        if (result.IsFailure)
+        {
+            return null;
+        }
+
+        return new Modules.Users.PublicApi.UserResponse(
+            result.Value.Id,
+            result.Value.Email,
+            result.Value.FirstName,
+            result.Value.LastName);
+    }
+}
