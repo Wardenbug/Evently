ba67c763dc7831dd58139a2c04ca48cfaee2a32e
Wardenbug <danylo.storozhuk.ua@gmail.com>
2025-07-19 15:52:38 +0200

    setting up keycloak provider

--- src/Modules/Ticketing/Evently.Modules.Ticketing.Infrastructure/TicketingModule.cs
+++ src/Modules/Ticketing/Evently.Modules.Ticketing.Infrastructure/TicketingModule.cs
@@ -1,25 +1,33 @@
 ﻿using Evently.Common.Infrastructure.Outbox;
 using Evently.Common.Presentation.Endpoints;
 using Evently.Modules.Ticketing.Application.Abstractions.Data;
+using Evently.Modules.Ticketing.Application.Abstractions.Payments;
 using Evently.Modules.Ticketing.Application.Carts;
 using Evently.Modules.Ticketing.Domain.Customers;
+using Evently.Modules.Ticketing.Domain.Events;
+using Evently.Modules.Ticketing.Domain.Orders;
+using Evently.Modules.Ticketing.Domain.Payments;
+using Evently.Modules.Ticketing.Domain.Tickets;
 using Evently.Modules.Ticketing.Infrastructure.Customers;
 using Evently.Modules.Ticketing.Infrastructure.Database;
-using Microsoft.Extensions.Configuration;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.EntityFrameworkCore.Migrations;
-using MassTransit;
-using Evently.Modules.Users.IntegrationEvents;
+using Evently.Modules.Ticketing.Infrastructure.Events;
+using Evently.Modules.Ticketing.Infrastructure.Orders;
+using Evently.Modules.Ticketing.Infrastructure.Payments;
+using Evently.Modules.Ticketing.Infrastructure.Tickets;
 using Evently.Modules.Ticketing.Presentation.Customers;
+using MassTransit;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Evently.Modules.Ticketing.Infrastructure;
 
 public static class TicketingModule
 {
     public static IServiceCollection AddTicketingModule(
-       this IServiceCollection services,
-       IConfiguration configuration)
+        this IServiceCollection services,
+        IConfiguration configuration)
     {
         services.AddInfrastructure(configuration);
 
@@ -33,8 +41,7 @@ public static class TicketingModule
         registrationConfigurator.AddConsumer<UserRegisteredIntegrationEventConsumer>();
     }
 
-    private static void AddInfrastructure(this IServiceCollection services,
-        IConfiguration configuration)
+    private static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddDbContext<TicketingDbContext>((sp, options) =>
             options
@@ -46,9 +53,15 @@ public static class TicketingModule
                 .UseSnakeCaseNamingConvention());
 
         services.AddScoped<ICustomerRepository, CustomerRepository>();
+        services.AddScoped<IEventRepository, EventRepository>();
+        services.AddScoped<ITicketTypeRepository, TicketTypeRepository>();
+        services.AddScoped<IOrderRepository, OrderRepository>();
+        services.AddScoped<ITicketRepository, TicketRepository>();
+        services.AddScoped<IPaymentRepository, PaymentRepository>();
 
         services.AddScoped<IUnitOfWork>(sp => sp.GetRequiredService<TicketingDbContext>());
 
         services.AddSingleton<CartService>();
+        services.AddSingleton<IPaymentService, PaymentService>();
     }
 }
