cf365ffc0978ee1f6571260ff0313e9f3c8d12d1
Wardenbug <danylo.storozhuk.ua@gmail.com>
2025-07-25 16:00:16 +0200

    add architecture tests

---
+++ tests/Evently.ArchitectureTests/Abstractions/TestResultExtension.cs
@@ -0,0 +1,12 @@
+﻿using FluentAssertions;
+using NetArchTest.Rules;
+
+namespace Evently.ArchitectureTests.Abstractions;
+
+internal static class TestResultExtension
+{
+    internal static void ShouldBeSuccessful(this TestResult result)
+    {
+        result.FailingTypes?.Should().BeEmpty();
+    }
+}
