86cd3a8138fd608c4f99dcf7ff2148865b9b9c17
Wardenbug <danylo.storozhuk.ua@gmail.com>
2025-07-30 23:14:53 +0200

    add materialized views

--- src/Modules/Attendance/Evently.Modules.Attendance.Infrastructure/Database/AttendanceDbContext.cs
+++ src/Modules/Attendance/Evently.Modules.Attendance.Infrastructure/Database/AttendanceDbContext.cs
@@ -19,6 +19,8 @@ public sealed class AttendanceDbContext(DbContextOptions<AttendanceDbContext> op
     internal DbSet<Event> Events { get; set; }
 
     internal DbSet<Ticket> Tickets { get; set; }
+    internal DbSet<EventStatistics> EventStatistics { get; set; }
+
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -31,5 +33,6 @@ public sealed class AttendanceDbContext(DbContextOptions<AttendanceDbContext> op
         modelBuilder.ApplyConfiguration(new AttendeeConfiguration());
         modelBuilder.ApplyConfiguration(new EventConfiguration());
         modelBuilder.ApplyConfiguration(new TicketConfiguration());
+        modelBuilder.ApplyConfiguration(new EventStatisticsConfiguration());
     }
 }
